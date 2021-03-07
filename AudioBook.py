1a27d2ae389ba1654d96b1236f8355c1887609c3
Marcus <63025671+Marcusdus@users.noreply.github.com>
2021-03-07 17:54:05 +0200

    Add files via upload

---
+++ AudioBook.py
@@ -0,0 +1,15 @@
+import pyttsx3
+import PyPDF2
+from tkinter.filedialog import *
+
+book = askopenfilename()
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(0, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    player = pyttsx3.init()
+    player.say(text)
+    player.runAndWait()
